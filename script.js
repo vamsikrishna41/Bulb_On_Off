e9f056cd9e19fc0d3df534ea55038d1851dc01af
Bathula Vamsi Krishna <121428816+vamsikrishna41@users.noreply.github.com>
2023-07-19 13:35:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function bulboff(){
+   document.getElementById("bulb").src="https://as2.ftcdn.net/v2/jpg/00/38/99/99/1000_F_38999921_dagMVem5FrDAgCJW4iU4lFhFmD44m2MG.jpg";
+}
+function bulbon(){
+      document.getElementById("bulb").src="https://www.freeiconspng.com/thumbs/bulb/clip-art-yellow-light-bulb-png-8.png";
+}
